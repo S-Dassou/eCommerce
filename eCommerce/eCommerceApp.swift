15ba5244ab726e140ec3888afc688dbc58e99ffb
Shafique Dassu <shafd1231@gmail.com>
2023-10-01 21:39:49 +0200

    Initial Commit

---
+++ eCommerce/eCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCommerceApp.swift
+//  eCommerce
+//
+//  Created by shafique dassu on 01/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct eCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
