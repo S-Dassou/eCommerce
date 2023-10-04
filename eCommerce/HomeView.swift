0aa0ad1a54b1a3accd82650b2a920b91e6a54750
Shafique Dassu <shafd1231@gmail.com>
2023-10-04 14:18:53 +0100

    hero page

--- eCommerce/HomeView.swift
+++ eCommerce/HomeView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    fileprivate func NavigationBarView() -> some View {
+        HStack {
+            Spacer()
+            Text("E-Commerce App")
+            Spacer()
+        }
+        .overlay(alignment: .trailing) {
+            Button(action: {
+                
+            }, label: {
+                ZStack {
+                    Image(systemName: "cart.fill")
+                        .foregroundStyle(Color.black)
+                    ZStack {
+                        Circle()
+                        Text("1")
+                            .font(.system(size: 14))
+                            .foregroundStyle(Color.white)
+                    }
+                    .offset(CGSize(width: 10, height: -10))
+                }
+            })
+            .padding(.trailing)
+        }
+    }
     var body: some View {
-        Text("Home View")
+        VStack {
+            NavigationBarView()
+            Image("banner")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 200)
+                .frame(width: .infinity)
+                .clipped()
+            Spacer()
+        }
     }
 }
 
