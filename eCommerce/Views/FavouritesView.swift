b09bd5a5208ea7c64433d968964471c0b4378ba5
Shafique Dassu <shafd1231@gmail.com>
2023-10-08 02:51:07 +0100

    Added list in favourites view using mock data Navigation Title for favourites view

--- eCommerce/Views/FavouritesView.swift
+++ eCommerce/Views/FavouritesView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct FavouritesView: View {
+    
+    @State var favourites: [Product] = Product.mockProducts
+    
     var body: some View {
-        Text("Favourites View")
+        NavigationStack {
+            VStack {
+                List(favourites) { favourite in
+                    HStack {
+                        Image(favourite.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 70, height: 70)
+                        VStack(alignment: .leading) {
+                            Text(favourite.title)
+                                .font(.system(size: 15))
+                                .padding(.bottom, 1)
+                            Text(favourite.description)
+                                .font(.system(size: 15))
+                                .padding(.bottom, 1)
+                                
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Favourites")
+        }
     }
 }
 
