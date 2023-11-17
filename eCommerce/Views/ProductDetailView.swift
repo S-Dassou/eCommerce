ff3dba494bc6d60b7cc1a01c05c83681015fa5b4
Shafique Dassu <shafd1231@gmail.com>
2023-11-17 22:54:40 +0000

    Added product detail view

--- eCommerce/Views/ProductDetailView.swift
+++ eCommerce/Views/ProductDetailView.swift
@@ -11,7 +11,35 @@ struct ProductDetailView: View {
     let product: Product
     var body: some View {
         
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Image(product.image)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
+                .frame(height: 200)
+                .clipped()
+            Text(product.title)
+                .font(.system(size: 20, weight: .medium))
+                .padding(.bottom, 5)
+            Text(product.description)
+                .font(.system(size: 15))
+                .padding(.bottom, 10)
+            Button(action: {
+                
+            }, label: {
+                Text("Add to cart")
+                    .font(.system(size: 15, weight: .bold))
+                    .foregroundStyle(Color.white)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 45)
+                    .background(Color.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                    .padding(.horizontal)
+                    
+            })
+            Spacer()
+        }
+        .padding(.horizontal)
     }
 }
 
