e8b0864e88a44a3066a5e8c98b59678188545088
Shafique Dassu <shafd1231@gmail.com>
2023-11-19 23:31:45 +0000

    cart display cart quantity and price vars set up product in cart model set up

--- eCommerce/Managers/CartManager.swift
+++ eCommerce/Managers/CartManager.swift
@@ -9,5 +9,18 @@ import Foundation
 import SwiftUI
 
 class CartManager: ObservableObject {
-    @Published var products: [Product] = []
+    @Published var productsInCart: [ProductInCart] = []
+    
+    var displayCartQuantity: Int {
+        return productsInCart.reduce(0) { $0 + $1.quantity }
+    }
+    
+    var displayTotalCartPrice: String {
+        let totalPrice = productsInCart.reduce(0) { $0 + ($1.product.price * Double($1.quantity)) }
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter.string(from: totalPrice as NSNumber) ?? "£0.00"
+        
+    }
 }
+
--- eCommerce/Models/ProductInCart.swift
+++ eCommerce/Models/ProductInCart.swift
@@ -8,7 +8,16 @@
 import Foundation
 
 struct ProductInCart: Identifiable {
-    let id = UUID()
+    var id: UUID {
+        return product.id
+    }
     let product: Product
     var quantity: Int
+    var DisplayTotalPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        let total = Double(quantity) * product.price
+        return formatter.string(from: total as NSNumber) ?? "£0.00"
+         
+    }
 }
